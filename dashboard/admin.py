6f93912c2d3281b56a474a347b034cdc178a4339
DanielMawioo <mawioodaniel43@gmail.com>
2020-04-06 12:41:25 +0300

    First commit

---
+++ dashboard/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['product_category', 'payment_method', 'shipping_cost', 'unit_price']
+
+admin.site.register(Order, OrderAdmin)
